5ed54f66057e102e38d5fb561d026f0d2731de46
eogus <kddhh2021@gmail.com>
2025-07-01 10:32:48 +0900

    implement getting  Transpose matrix

--- chapter2/transpose.py
+++ chapter2/transpose.py
@@ -1,4 +1,6 @@
-# 전치행렬을 구하는 코드
+### 전치행렬을 구하는 코드
+
+# 알고리즘
 # 1. 행렬의 크기 파악 (run only when, rows >= 2 and cols >= 2)
 #    row size → len(input), col size → len(input[0])
 #    혹은 numpy의 shape attribute 이용하기 → input.shape (튜플 형태)
@@ -8,10 +10,32 @@
 
 import numpy as np
 
-input_M = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12]])
-input_row = len(input_M)
-input_col = len(input_M[0])
+def getTranspose(np_array):
+    origin_row = len(np_array)
+    origin_col = len(np_array[0])
 
+    if (origin_row < 2 or origin_col < 2):
+        print(f"minimun matrix size is 2x2")
+        return -1
+    else:
+        T = np.empty((origin_col, origin_row), dtype=np_array.dtype)
+        for row in range(origin_row):
+            for col in range(origin_col):
+                T[col][row] = np_array[row][col]
+        
+        return T
+    
+
+input_M = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12]])
+
+print(f"input matrix shape is {input_M.shape}")
 print("input matrix is ...")
 print(input_M)
-print(f"input matrix's row is {input_row} and col size is {input_col}")
+print()
+
+output = getTranspose(input_M)
+
+print(f"output matrix shape is {output.shape}")
+print("output matrix is ...")
+print(output)
+print()
